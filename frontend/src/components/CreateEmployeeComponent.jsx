afd99bfc0a26c64be5312467b583fd1902fee5c5
Duc Nguyen <ngminhduc20111997@gmail.com>
2021-03-12 20:10:07 -0800

    Frontend: Finish POST endpoint

--- frontend/src/components/CreateEmployeeComponent.jsx
+++ frontend/src/components/CreateEmployeeComponent.jsx
@@ -1,10 +1,108 @@
 import React, { Component } from "react";
+import EmployeeService from "../services/EmployeeService.js";
 
 class CreateEmployeeComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstName: "",
+      lastName: "",
+      emailId: "",
+    };
+    this.changeFirstNameHandler = this.changeFirstNameHandler.bind(this);
+    this.changeLastNameHandler = this.changeLastNameHandler.bind(this);
+    this.changeEmailIdHandler = this.changeEmailIdHandler.bind(this);
+    this.createEmployeeHandler = this.createEmployeeHandler.bind(this);
+    this.cancelHandler = this.cancelHandler.bind(this);
+  }
+
+  changeFirstNameHandler = (event) => {
+    this.setState({ firstName: event.target.value });
+  };
+
+  changeLastNameHandler = (event) => {
+    this.setState({ lastName: event.target.value });
+  };
+
+  changeEmailIdHandler = (event) => {
+    this.setState({ emailId: event.target.value });
+  };
+
+  createEmployeeHandler = (event) => {
+    event.preventDefault();
+    let employee = {
+      firstName: this.state.firstName,
+      lastName: this.state.lastName,
+      emailId: this.state.emailId,
+    };
+    console.log("Employee created: " + JSON.stringify(employee));
+
+    EmployeeService.createEmployee(employee).then((result) => {
+      this.props.history.push("/employees");
+    });
+  };
+
+  cancelHandler() {
+    this.props.history.push("/employees");
+  }
+
   render() {
     return (
       <div>
-        <h1>Employee Form</h1>
+        <div className="container">
+          <div className="row">
+            <div className="card col-md-6 offset-md-3 offset-md-3">
+              <h3 className="text-center">Add Employee</h3>
+              <div className="card-body">
+                <form>
+                  <div className="form-group">
+                    <label>First Name:</label>
+                    <input
+                      placeholder="John"
+                      name="firstName"
+                      className="form-control"
+                      value={this.state.firstName}
+                      onChange={this.changeFirstNameHandler}
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label>Last Name:</label>
+                    <input
+                      placeholder="Snow"
+                      name="lastName"
+                      className="form-control"
+                      value={this.state.lastName}
+                      onChange={this.changeLastNameHandler}
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label>Email:</label>
+                    <input
+                      placeholder="JohnSnow@gmail.com"
+                      name="emailId"
+                      className="form-control"
+                      value={this.state.emailId}
+                      onChange={this.changeEmailIdHandler}
+                    />
+                  </div>
+                  <button
+                    className="btn btn-success"
+                    onClick={this.createEmployeeHandler}
+                  >
+                    Create
+                  </button>
+                  <button
+                    className="btn btn-danger"
+                    onClick={this.cancelHandler}
+                    style={{ marginLeft: "10px" }}
+                  >
+                    Cancel
+                  </button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
