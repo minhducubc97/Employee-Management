79e8c31e0817eeeb53418fe477b40446ac82cf6b
Duc Nguyen <ngminhduc20111997@gmail.com>
2021-03-12 20:37:28 -0800

    Backend: Finished GET by ID and PUT endpoints

--- backend/src/main/java/com/ducnguyen/employeemanagement/controller/EmployeeController.java
+++ backend/src/main/java/com/ducnguyen/employeemanagement/controller/EmployeeController.java
@@ -1,8 +1,10 @@
 package com.ducnguyen.employeemanagement.controller;
 
+import com.ducnguyen.employeemanagement.exception.NotFoundException;
 import com.ducnguyen.employeemanagement.model.Employee;
 import com.ducnguyen.employeemanagement.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,8 +22,26 @@ public class EmployeeController {
         return employeeRepository.findAll();
     }
 
+    @GetMapping("/employees/{id}")
+    public ResponseEntity<Employee> getEmployee(@PathVariable Long id) {
+        Employee employee = employeeRepository.findById(id).orElseThrow(() -> new NotFoundException("Employee" + id + "not found!"));
+        return ResponseEntity.ok(employee);
+    }
+
     @PostMapping("/employees")
     public Employee createEmployee(@RequestBody Employee employee) {
         return employeeRepository.save(employee);
+    }
+
+    @PutMapping("/employees/{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable Long id, @RequestBody Employee employeeToUpdate) {
+        Employee employee =
+                employeeRepository.findById(id).orElseThrow(() -> new NotFoundException("Employee" + id + "not " +
+                        "found!"));
+        employee.setFirstName(employeeToUpdate.getFirstName());
+        employee.setLastName(employeeToUpdate.getLastName());
+        employee.setEmailId(employeeToUpdate.getEmailId());
+        Employee employeeUpdated = employeeRepository.save(employee);
+        return ResponseEntity.ok(employeeUpdated);
     }
 }
